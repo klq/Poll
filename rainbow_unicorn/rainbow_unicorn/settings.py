4a83c7b3dda395ecc3fed5ef73842b6fc3dc3781
The Diabolical Lingqiang Kong <konglq@gmail.com>
2013-06-01 10:42:06 +0000

    created sqlite3 db and created app poll models

--- rainbow_unicorn/rainbow_unicorn/settings.py
+++ rainbow_unicorn/rainbow_unicorn/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/bostonuniversity/mydb/lighting/', # Or path to database file if using sqlite3.
+        'NAME': '/Users/bostonuniversity/mydb/lightening.sql', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
