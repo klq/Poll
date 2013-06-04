4b741f49f41fbc826747f6d5190894cdab647aca
The Diabolical Lingqiang Kong <konglq@gmail.com>
2013-06-04 21:38:16 +0000

    finished part 1 of tutorial

--- rainbow_unicorn/polls/models.py
+++ rainbow_unicorn/polls/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Poll(models.Model):
+  from django.utils import timezone
   question = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
 
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=4)
+
   def __unicode__(self):
     return self.question
-
-  def was_published_recently(self):
-    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
   poll = models.ForeignKey(Poll)
